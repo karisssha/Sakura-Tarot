286a94dafe60d24ae6607418eafe805a4d9ac3e5
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-02-12 15:48:47 +0100

    create footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   console.log("Footer renderizado");
   return (
-    <footer className="footer-container" style={{ backgroundColor: "lightgray", padding: "20px" }}>
+    <footer className="footer-container">
       <p>About Us © 2025 Sakura Cards Tarot Github</p>
     </footer>
   );
