2a0ac5b0ce71e06d3110e14393f1d26b0c46ea3f
karisssha <krshmelendez@gmail.com>
2025-02-12 23:47:06 +0100

    created feature-pageCards

---
+++ src/pages/pickCards/Cards.jsx
@@ -0,0 +1,17 @@
+import styles from './Cards.module.css'
+import TextCloud from '../../assets/img/cloud.webp'
+
+function Cards() {
+  
+    return (
+      <>
+        <TextCloud/>
+        <div className={styles.cardsDeck}>
+          
+        </div>
+      </>
+    )
+  }
+  
+  export default Cards
+  
