af8afc5b5f2b4cb92d8ea5357777b5ab5877221a
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-02-12 14:15:22 +0100

    add containers

--- src/pages/read/Read.jsx
+++ src/pages/read/Read.jsx
@@ -5,10 +5,31 @@ function Read() {
 
     return (
     <>
+    <section class="cred">
+    <div>Input Name</div>
+    <div>Date</div>
+    </section>
+
+    <div class="card-read">
+
+    </div>
         
 
 
+    <div class="card-read">
 
+    </div>
+
+
+    <div class="card-read">
+
+    </div>
+
+    <section>
+      <button>see history</button>
+      <button><img src="./git status
+      src/assets/img/keroIcon.png"></img>save reading</button>
+    </section>
 
 
 
