d171205e0ac2b72aafa186821a74601cd9afa752
saudlari <larissa@riseup.net>
2025-02-13 01:21:36 +0100

    FetchCards with map and formatCard

--- src/services/apiCards.jsx
+++ src/services/apiCards.jsx
@@ -5,7 +5,16 @@ const API_URL = "https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/";
 export const FetchCards = async () => {
   try {
     const response = await axios.get(API_URL);
-    return response.data;
+    
+    const formatCard = response.data.map(card => ({
+      id: card.id,
+      cardsReverse: card.cardsReverse,
+      englishName: card.englishName,
+      meaning: card.meaning,
+      sakuraCard: card.sakuraCard
+    }));
+    
+    return formatCard;
   } catch (error) {
     console.error("Error fetching cards:", error);
     throw error;
