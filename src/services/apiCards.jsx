1bb159fcc1de9ef2ec853be3e7c99722c931bddd
saudlari <larissa@riseup.net>
2025-02-13 01:18:35 +0100

    apiService with async and get axios

---
+++ src/services/apiCards.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_URL = "https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/";
+
+export const FetchCards = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching cards:", error);
+    throw error;
+  }
+};
+
+
