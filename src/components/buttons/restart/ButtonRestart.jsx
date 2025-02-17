fef335f25aee696051b8b06f024c905eeb06de9f
karisssha <krshmelendez@gmail.com>
2025-02-17 17:59:14 +0100

    added restart functions to button

--- src/components/buttons/restart/ButtonRestart.jsx
+++ src/components/buttons/restart/ButtonRestart.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 import styles from './ButtonRestart.module.css'
 
 
 const ButtonRestart = () => {
-  const handleClick = () => {
-    window.location.reload();
-  };
+const navigate = useNavigate();
+const location = useLocation();
 
-  return (
-    <button className={styles.Restart} onClick={handleClick}> 
-      <p className= {styles.Text}>restart</p>
-      <img className= {styles.SakuraFlower} src="/src/assets/img/sakuraFlower.png" alt="Restart Button" />
-    </button>
-  );
+const handleClick = () => {
+if (location.pathname === '/cards') {
+window.location.reload(); 
+} else {
+navigate('/cards');
+}
+};
+
+return (
+<button className={styles.Restart} onClick={handleClick}> 
+<p className= {styles.Text}>restart</p>
+<img className= {styles.SakuraFlower} src="/src/assets/img/sakuraFlower.png" alt="Restart Button" />
+</button>
+);
 };
 
 export default ButtonRestart;
